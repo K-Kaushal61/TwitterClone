ce5e5dd3634dbadaa0636c7bba2dc00d1015c6b1
K-Kaushal61 <130486659+K-Kaushal61@users.noreply.github.com>
2024-08-18 22:34:56 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,29 @@
+export const tweets = [
+  {
+    id: 0,
+    profilePicture: "assets/images/user-profil.jpg",
+    name: "Kartik Kaushal",
+    username: "@kartikkaushal",
+    date: "1h",
+    text: "I had gone out to take photos early in the morning, and it was my lucky day!",
+    image: "assets/images/tw-pic-2.png",
+  },
+  {
+    id: 1,
+    profilePicture: "assets/images/tw-github.png",
+    name: "GitHub",
+    username: "@github",
+    date: "2h",
+    text: "How are you protecting your organization from security threats? Join us at #OfficialCyberSummit to learn how GitHub can help.",
+    image: "assets/images/tw-pic-1.png",
+  },
+  {
+    id: 2,
+    profilePicture: "assets/images/tw-deno.png",
+    name: "AIALIEN",
+    username: "@Alien",
+    date: "4d",
+    text: "Got a question for one of our engineers? Come ask it at our next Office Hours.",
+    image: "",
+  },
+];
